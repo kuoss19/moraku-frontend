b7ce5a5757fe16ccebe8404ad0efa86a47133400
heejin <qkrgmlwls104@gmail.com>
2019-12-09 17:19:04 +0900

    Initialize Component

---
+++ src/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style.css';
+import './logo.jpg';
+
+function Logo() {
+  return <img src="./logo.jpg" alt="logo" className="logo"></img>;
+}
+
+export default Logo;
